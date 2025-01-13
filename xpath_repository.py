727d2f54ca09f0ba20689f2de60c4fcd54a02a2c
yunsammy <yun3.jo@gmail.com>
2025-01-13 12:26:17 +0900

    Add Xpath repository

---
+++ xpath_repository.py
@@ -0,0 +1,7 @@
+XPATHS = {
+    "ad_renderer": "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-shorts/div[3]/div[2]/ytd-reel-video-renderer[{video_order}]/div[4]/ytd-ad-slot-renderer",
+    "like_button": "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-shorts/div[3]/div[2]/ytd-reel-video-renderer[{video_order}]/div[4]/ytd-reel-player-overlay-renderer/div[2]/div/div[1]/ytd-like-button-renderer/ytd-toggle-button-renderer[1]/yt-button-shape/label/div/span",
+    "comment_count": "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-shorts/div[3]/div[2]/ytd-reel-video-renderer[{video_order}]/div[4]/ytd-reel-player-overlay-renderer/div[2]/div/div[2]/ytd-button-renderer/yt-button-shape/label/div/span",
+    "username": "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-shorts/div[3]/div[2]/ytd-reel-video-renderer[{video_order}]/div[4]/ytd-reel-player-overlay-renderer/div[1]/div[1]/div/yt-reel-metapanel-view-model/div[1]/yt-reel-channel-bar-view-model/span/a",
+    "next_video_btn": "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-shorts/div[5]/div[2]/ytd-button-renderer/yt-button-shape/button"
+}
