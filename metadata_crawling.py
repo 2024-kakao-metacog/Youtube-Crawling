c6e6399b0858e159d808fcea8f00cf9d3b05a13e
yunsammy <yun3.jo@gmail.com>
2025-01-13 16:55:48 +0900

    headless option 추가(브라우저 창 없이 작동)

--- metadata_crawling.py
+++ metadata_crawling.py
@@ -137,7 +137,7 @@ def initiate_driver():
     
     # Selenium Chrome options
     options = Options()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=860,540")
